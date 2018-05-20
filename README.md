ce740531ba4c428c4ad6e8fd06cac06c73096346
SubhamTyagi <shubhamtyagi.bajoat@gmail.com>
2018-05-20 17:01:58 +0530

    first commit
    20june2018/2.1

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# loco-hack
-This is script for finding answer for loco app(Trivia Game), Only for Educational purpose
+# LOCO_HACK
+![License: MIT][ico-license]
+
+## INCOMPLETE!!
+## RELASING SOON...
+
+A bot to help answer questions on trivia apps like LOCO. This bot takes screenshot of the game from phone and uses googles tesseract OCR to read the questions and options. It automates the process of googling of the answers and gives the most likely answer! It is 85%+ accurate!
+
+Since it is against the policy of LOCO i do not encourage anyone to use this during a live game and this is purely for educational purposes.  
+
+## Packages Used
+
+Use python 2.7  or you can use python37(some compatibility issues )particular the packages/libraries used are...
+
+* JSON - Data Storage
+* Pillow - Image manipulation
+* Google-Search-API - Google searching
+* pytesseract - Google's free/open source OCR (requires separate installation)
+* beautifulsoup4 - Parse google searches/html
+* lxml - Beautifulsoup parser
+* opencv2 - Image manipulation
+* pyscreenshot - Take screenshot of the game
+
+
+*To easily install these*
+1. Install python 2.7
+2. Install above packages
+    * `$ pip install -r requirements.txt`
+3. For tesseract
+    * `$ brew install tesseract`
+4. For opencv
+    * `$ brew install opencv`
+5. Android adb
+    * `Either you have android sdk or minimal ADB`
+
+## Usage
+
+Make sure all packages above are installed. For android phones use, To use this script :
+
+```bash
+$ git clone https://github.com/SubhamTyagi/LOCO_HACK
+$ cd LOCO_HACK
+$ pip install -r requirements.txt
+$ python bot.py
+
+Press s or Enter to screenshot live game, d to run against sample questions or q to quit:
+s
+...Question...
+```
+## Screenshots
+
+
+
+
+## Contributing
+
+All contributions welcome.
+
+## Credits
+- [SHUBHAM TYAGI][link-author]
+
+- [All Contributors][link-contributors]
+
+## Useful links
+
+- [Wikipedia-API][link-wikiapi]
+- [Google-Search-API][link-gapi]
+- [Tesseract][link-tesseract]
+
+## License
+
+The MIT License (MIT)
+
+[ico-license]: https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square
+
+[link-author]: https://github.com/SubhamTyagi/
+
+[link-contributors]: ../../contributors
+[link-wikiapi]: https://pypi.python.org/pypi/wikipedia
+[link-gapi]: https://github.com/abenassi/Google-Search-API
+[link-mike]: https://github.com/mikealmond/hq-trivia-assistant
+[link-tesseract]: https://github.com/tesseract-ocr/tesseract/wiki
+[sampq]: ()
---
+++ bot.py
@@ -0,0 +1,283 @@
+"""
+
+    TODO:
+    * Implement normalize function
+    * Attempt to google wiki \"...\" part of question
+    * Rid of common appearances in 3 options
+    * Automate screenshot process
+    * Implement Asynchio for concurrency
+    //Script is in working condition at all times with python27
+    //TODO is for improving accuracy
+
+"""
+
+# Answering bot for LOCO
+import json
+import urllib2
+from bs4 import BeautifulSoup
+from google import google
+from PIL import Image
+import pytesseract
+import argparse
+import cv2
+import os
+import sys
+
+from halo import Halo
+
+
+# for terminal colors
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+# sample questions from previous games
+sample_questions = {}
+
+# list of words to clean from the question during google search
+remove_words = []
+
+# negative words
+negative_words = []
+
+
+# load sample questions
+def load_json():
+    global remove_words, sample_questions, negative_words
+    remove_words = json.loads(open("Data/settings.json").read())["remove_words"]
+    negative_words = json.loads(open("Data/settings.json").read())["negative_words"]
+    sample_questions = json.loads(open("Data/questions.json").read())
+
+
+# simplify question and remove which,what....etc //question is string
+def simplify_ques(question):
+    neg = False
+    qwords = question.lower().split()
+    if [i for i in qwords if i in negative_words]:
+        neg = True
+    cleanwords = [word for word in qwords if word.lower() not in remove_words]
+    temp = ' '.join(cleanwords)
+    clean_question = ""
+    # remove ?
+    for ch in temp:
+        if ch != "?" or ch != "\"" or ch != "\'":
+            clean_question = clean_question + ch
+
+    return clean_question.lower(), neg
+
+
+# get web page
+def get_page(link):
+    try:
+        if link.find('mailto') != -1:
+            return ''
+        req = urllib2.Request(link, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'})
+        html = urllib2.urlopen(req).read()
+        return html
+    except (urllib2.URLError, urllib2.HTTPError, ValueError) as e:
+        return ''
+
+
+# split the string
+def split_string(source):
+    splitlist = ",!-.;/?@ #"
+    output = []
+    atsplit = True
+    for char in source:
+        if char in splitlist:
+            atsplit = True
+        else:
+            if atsplit:
+                output.append(char)
+                atsplit = False
+            else:
+                output[-1] = output[-1] + char
+    return output
+
+
+# answer by combining two words
+def smart_answer(content, qwords):
+    zipped = zip(qwords, qwords[1:])
+    points = 0
+    for el in zipped:
+        if content.count(el[0] + " " + el[1]) != 0:
+            points += 1000
+    return points
+
+
+# use google to get wiki page
+def google_wiki(sim_ques, options, neg):
+    spinner = Halo(text='Googling. and searching Wikipedia', spinner='dots2')
+    spinner.start()
+    # number of google result pages
+    num_pages = 1
+    points = list()
+    content = ""
+    maxo = ""
+    maxp = -sys.maxsize
+    words = split_string(sim_ques)
+    for o in options:
+
+        o = o.lower()
+        original = o
+        o += ' wiki'
+
+        # get google search results for option + 'wiki'
+        search_wiki = google.search(o, num_pages)
+
+        link = search_wiki[0].link
+        content = get_page(link)
+        soup = BeautifulSoup(content, "lxml")
+        page = soup.get_text().lower()
+
+        temp = 0
+
+        for word in words:
+            temp = temp + page.count(word)
+        temp += smart_answer(page, words)
+        if neg:
+            temp *= -1
+        points.append(temp)
+        if temp > maxp:
+            maxp = temp
+            maxo = original
+    spinner.succeed()
+    spinner.stop()
+    return points, maxo
+
+
+# return points for sample_questions
+def get_points_sample():
+    simq = ""
+    x = 0
+    for key in sample_questions:
+        x = x + 1
+        points = []
+        simq, neg = simplify_ques(key)
+        options = sample_questions[key]
+        simq = simq.lower()
+        maxo = ""
+        points, maxo = google_wiki(simq, options, neg)
+        print("\n" + str(x) + ". " + bcolors.UNDERLINE + key + bcolors.ENDC + "\n")
+        for point, option in zip(points, options):
+            if maxo == option.lower():
+                option = bcolors.OKGREEN + option + bcolors.ENDC
+            print(option + " { points: " + bcolors.BOLD + str(point) + bcolors.ENDC + " }\n")
+
+
+# take screenshot from phone and return text from that screenshot if it is not demo
+def take_screenshot_and_get_text(demo):
+    # screenshot_file =''
+
+    if demo:
+        screenshot_file = "screendemo.png"
+    else:
+        os.system("adb exec-out screencap -p > screen.png")
+        screenshot_file = "screen.png"
+
+    spinner = Halo(text='Reading screen', spinner='bouncingBar')
+    spinner.start()
+
+    # prepare argparse
+    ap = argparse.ArgumentParser(description='LOCO_BOT')
+    ap.add_argument("-i", "--image", required=False, default=screenshot_file, help="path to input image to be OCR'd")
+    ap.add_argument("-p", "--preprocess", type=str, default="thresh", help="type of preprocessing to be done")
+    args = vars(ap.parse_args())
+
+    # load the image
+    image = cv2.imread(args["image"])
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    if args["preprocess"] == "thresh":
+        gray = cv2.threshold(gray, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    elif args["preprocess"] == "blur":
+        gray = cv2.medianBlur(gray, 3)
+
+    # store grayscale image as a temp file to apply OCR
+    filename = "screen{}.png".format(os.getpid())
+    cv2.imwrite(filename, gray)
+
+    # load the image as a PIL/Pillow image, apply OCR, and then delete the temporary file
+    text = pytesseract.image_to_string(Image.open(filename))
+    # os.remove(filename)
+    # os.remove(screenshot_file)
+
+    # show the output images
+
+    '''cv2.imshow("Image", image)
+    cv2.imshow("Output", gray)
+    os.remove(screenshot_file)
+    if cv2.waitKey(0):
+        cv2.destroyAllWindows()
+    print(text)
+    '''
+    spinner.succeed()
+    spinner.stop()
+    return text
+
+
+# get questions and options from OCR text
+def parse_question(demo):
+    text = take_screenshot_and_get_text(demo)
+    lines = text.splitlines()
+    question = ""
+    options = list()
+    flag = False
+
+    for line in lines:
+        if not flag:
+            question = question + " " + line
+
+        if '?' in line:
+            flag = True
+            continue
+
+        if flag:
+            if line != '':
+                options.append(line)
+
+    return question, options
+
+
+# return points for live game // by screenshot
+def get_points_live(demo):
+    neg = False
+    question, options = parse_question(demo)
+    simq = ""
+    points = []
+    simq, neg = simplify_ques(question)
+    maxo = ""
+    m = 1
+    if neg:
+        m = -1
+    points, maxo = google_wiki(simq, options, neg)
+    print("\n" + bcolors.UNDERLINE + question + bcolors.ENDC + "\n")
+    for point, option in zip(points, options):
+        if maxo == option.lower():
+            option = bcolors.OKGREEN + option + bcolors.ENDC
+        print(option + " { points: " + bcolors.BOLD + str(point * m) + bcolors.ENDC + " }\n")
+
+
+# menu// main func
+if __name__ == "__main__":
+    load_json()
+    while (1):
+        keypressed = raw_input(
+            bcolors.WARNING + '\nPress s to screenshot live game, demo to run against sample questions '
+                              'or q to quit:\n' + bcolors.ENDC)
+        if keypressed == 's':
+            get_points_live(False)
+        elif keypressed == 'd':
+            get_points_live(True)
+        elif keypressed == 'q':
+            break
+        else:
+            get_points_live(False)
+            print(bcolors.FAIL + "\nUnknown input" + bcolors.ENDC)
